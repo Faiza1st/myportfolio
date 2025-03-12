7416ce39aea20b1b5c0a2b770fb5768f5d5996f9
Faiza Faisal <150220093+Faiza1st@users.noreply.github.com>
2025-03-12 12:39:33 +1100

    Update banner.js

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,7 +1,8 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assests/profilepicture.png";
 import { ArrowRightCircle, EmojiLaughing } from "react-bootstrap-icons";
+
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
@@ -10,44 +11,50 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const toRotate = useMemo(() => ["Web Developer", "Web Designer", "Front End Developer"], []);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Transdisciplinary Innovator", "Business Strategist", "Web Developer"];
   const period = 2000;
 
   useEffect(() => {
-    const tick = () => {
-      const i = loopNum % toRotate.length;
-      const fullText = toRotate[i];
-      const updatedText = isDeleting
-        ? fullText.substring(0, text.length - 1)
-        : fullText.substring(0, text.length + 1);
-
-      setText(updatedText);
-
-      if (isDeleting) {
-        setDelta((prevDelta) => prevDelta / 2);
-      }
-
-      if (!isDeleting && updatedText === fullText) {
-        setIsDeleting(true);
-        setDelta(period);
-      } else if (isDeleting && updatedText === "") {
-        setIsDeleting(false);
-        setLoopNum((prevLoopNum) => prevLoopNum + 1);
-        setDelta(500);
-      }
-    };
-
-    const ticker = setInterval(() => {
+    let ticker = setInterval(() => {
       tick();
     }, delta);
 
-    return () => clearInterval(ticker);
-  }, [text, delta, isDeleting, loopNum, toRotate]);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="align-items-center">
+        <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) => (
@@ -58,29 +65,30 @@ export const Banner = () => {
                 >
                   <span className="tagline">
                     Welcome !!! <EmojiLaughing size={25} />{" "}
-                    <EmojiLaughing size={25} />
+                    <EmojiLaughing size={25} />{" "}
                   </span>
                   <h1>
                     {`I'm Faiza Faisal, `}{" "}
                     <span
                       className="txt-rotate"
                       dataPeriod="1000"
-                      data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
+                      data-rotate='[ "Transdisciplinary Innovator", "Business Strategist", "Web Developer" ]'
                     >
                       <span className="wrap">{text}</span>
                     </span>
                   </h1>
                   <p>
-                     A business strategist with a passion for web development. I blend strategic thinking with digital innovation to create meaningful, user-centered solutions. With expertise in business strategy and hands-on skills in HTML, CSS, JavaScript, and React, I focus on crafting digital experiences that truly make an impact.
+                  A business strategist with a passion for web development. I blend strategic thinking with digital innovation to create meaningful, user-centered solutions. With expertise in business strategy and hands-on skills in HTML, CSS, JavaScript, and React, I focus on crafting digital experiences that truly make an impact.
 
 What sets me apart is my transdisciplinary approach—bringing together insights from business, technology, and design to solve complex challenges. I thrive in collaborative environments, where I can bridge the gap between strategy and execution to deliver sustainable, scalable solutions.
 
 I’m get excited to work on projects that demand fresh, cross-disciplinary thinking and a real commitment to creating tangible, lasting results.{" "}
                   </p>
+                  {/* <button onClick={() => console.log("connect")}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button> */}
                   <a href="tel:+61490050656" className="vvd">
-                    <button>
-                      Let’s Connect <ArrowRightCircle size={25} />
-                    </button>
+                    <button>Let’s Connect <ArrowRightCircle size={25} /></button>
                   </a>
                 </div>
               )}
@@ -94,7 +102,7 @@ I’m get excited to work on projects that demand fresh, cross-disciplinary thin
                     isVisible ? "animate__animated animate__zoomIn" : ""
                   }
                 >
-                  <img src={headerImg} alt="Faiza Faisal's profile" />
+                  <img src={headerImg} alt="Header Img" />
                 </div>
               )}
             </TrackVisibility>
